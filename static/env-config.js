daee949d438c54941334119494adb60ce52c63e4
4Sparkz <109384199+4Sparkz@users.noreply.github.com>
2023-09-04 13:32:11 +0100

    Add files via upload

--- static/env-config.js
+++ static/env-config.js
@@ -1,5 +1,6 @@
 window._env_ = {
-  REACT_APP_BASE_DIRECTORY: "/amiibo",
+  REACT_APP_BASE_DIRECTORY: "/",
   REACT_APP_API_BASE_URL: "https://",
-  PUBLIC_URL: "/amiibo",
-}
+  PUBLIC_URL: "/",
+};
+
